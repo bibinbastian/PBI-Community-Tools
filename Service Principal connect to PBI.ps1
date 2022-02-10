036c01576783a7541e38f0447505eeddd90df864
bibinbastian <bibinbastian@outlook.com>
2022-02-10 07:06:24 -0800

    Added Service Principal connect to PBI

---
+++ Service Principal connect to PBI.ps1
@@ -0,0 +1,14 @@
+﻿Set-PSRepository PSGallery -InstallationPolicy Trusted
+Install-Module -Name MicrosoftPowerBIMgmt -Scope CurrentUser
+Import-Module -Name MicrosoftPowerBIMgmt
+ $tenant_id = "##"
+ $client_id = "##"
+ $client_secret = "##"
+ 
+ [securestring]$sec_client_secret = ConvertTo-SecureString $client_secret -AsPlainText -Force
+ [pscredential]$credential = New-Object System.Management.Automation.PSCredential ($client_id, $sec_client_secret)
+ Connect-PowerBIServiceAccount -Credential $credential -ServicePrincipal -TenantId $tenant_id
+ Write-Information "Connected"
+
+ 
+ 
